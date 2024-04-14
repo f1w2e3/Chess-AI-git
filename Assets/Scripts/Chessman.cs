5c41a1d3f2be236887e63276ade28e576f048949
WaterGoPotato <85994711+WaterGoPotato@users.noreply.github.com>
2024-04-14 17:18:53 +0900

    Game, Chessman스크립트
    Game스크립트의 Start()함수와 Create()함수,
    Chessman 스크립트의 Active()와 SetCord() 함수 를 유투브를 참고하여 작성

--- Assets/Scripts/Chessman.cs
+++ Assets/Scripts/Chessman.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class Chessman : MonoBehaviour
@@ -16,8 +17,56 @@ private int yBoard = -1;
 private string player;
 
 
-public Sprite black_queen, black_kinght, black_bishop, black_king, black_rook, black_pawn;
+public Sprite black_queen, black_knight, black_bishop, black_king, black_rook, black_pawn;
 public Sprite white_queen, white_knight, white_bishop, white_king, white_rook, white_pawn;
 
+
+//기물과 관련된 초기 설정
+public void Active()
+{
+    controller = GameObject.FindGameObjectWithTag("GameController");
+
+    SetCords();
+
+    switch(this.name)
+    {
+        case "black_queen": this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
+        case "black_knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
+        case "black_bishop": this.GetComponent<SpriteRenderer>().sprite = black_bishop; break;
+        case "black_king": this.GetComponent<SpriteRenderer>().sprite = black_king; break;
+        case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
+        case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
+
+        case "white_queen": this.GetComponent<SpriteRenderer>().sprite = white_queen; break;
+        case "white_knight": this.GetComponent<SpriteRenderer>().sprite = white_knight; break;
+        case "white_bishop": this.GetComponent<SpriteRenderer>().sprite = white_bishop; break;
+        case "white_king": this.GetComponent<SpriteRenderer>().sprite = white_king; break;
+        case "white_rook": this.GetComponent<SpriteRenderer>().sprite = white_rook; break;
+        case "white_pawn": this.GetComponent<SpriteRenderer>().sprite = white_pawn; break;
+    }
 }
+
+//기물의 위치를 조정해주는 함수
+public void SetCords()
+    {
+        float x = xBoard;
+        float y = yBoard;
+
+        x *= 0.675f; x += -2.35f;
+        y *= 0.68f; y += -2.8f;
+
+        this.transform.position = new Vector3(x,y,-1.0f);
+    }   
+
+
+    public int GetXBoard() {return xBoard;} 
+    public int GetYBoard() { return yBoard;}
+    public void SetXBoard(int x){ xBoard = x;}
+    public void SetYBoard(int y){ yBoard = y;}
+}
+
+
+
+
+
 
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,19 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Game : MonoBehaviour
 {
     public GameObject chesspiece; //체스 기물
 
+    private GameObject[,] boardPositions = new GameObject[8,8]; // 기물들의 위치를 저장
+    private GameObject[] playerBlack = new GameObject[16]; 
+    private GameObject[] playerWhite = new GameObject[16];
+
+
+    private string currentPlayer = "white";
+
+    private bool gameOver = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-      Instantiate(chesspiece, new Vector3(0,0,-1), Quaternion.identity); 
-        /*chesspiece에게 위치를 주는 함수.
-        z값이 -1처럼 음수여야 체스 보드판 위로 기물이 올라온다*/
+      /*
+      GameObject testObj = Instantiate(chesspiece, new Vector3(0,0,-1), Quaternion.identity); 
+        //chesspiece에게 위치를 주는 함수.
+        //z값이 -1처럼 음수여야 체스 보드판 위로 기물이 올라온다
+      */
+
+      //기물들의 초기위치 설정 밑  생성
+      playerWhite = new GameObject[] 
+      {
+        Create("white_rook",0,0), Create("white_knight",1,0), Create("white_bishop",2,0),
+        Create("white_queen",3,0), Create("white_king",4,0), Create("white_bishop",5,0),
+        Create("white_knight",6,0), Create("white_rook",7,0), 
+        Create("white_pawn",0,1), Create("white_pawn",1,1), Create("white_pawn",2,1),
+        Create("white_pawn",3,1), Create("white_pawn",4,1), Create("white_pawn",5,1),
+        Create("white_pawn",6,1), Create("white_pawn",7,1)
+      };
+      playerBlack = new GameObject[]
+      {
+        Create("black_rook",0,7), Create("black_knight",1,7), Create("black_bishop",2,7),
+        Create("black_queen",3,7), Create("black_king",4,7), Create("black_bishop",5,7),
+        Create("black_knight",6,7), Create("black_rook",7,7), 
+        Create("black_pawn",0,6), Create("black_pawn",1,6), Create("black_pawn",2,6),
+        Create("black_pawn",3,6), Create("black_pawn",4,6), Create("black_pawn",5,6),
+        Create("black_pawn",6,6), Create("black_pawn",7,6)
+      };
+
+      for (int i = 0; i < playerBlack.Length; i++)
+      {
+        SetPosition(playerBlack[i]);
+        SetPosition(playerWhite[i]);
+      }
     }
 
+  public GameObject Create(string name, int x, int y)
+  {
+    GameObject obj = Instantiate(chesspiece, new Vector3(0,0,-1), Quaternion.identity);
+    Chessman cm = obj.GetComponent<Chessman>();
+    cm.name = name;
+    cm.SetXBoard(x);
+    cm.SetYBoard(y);
+    cm.Active();
+    return obj;
+  }
     
+    public void SetPosition(GameObject obj)
+    {
+      Chessman cm = obj.GetComponent<Chessman>();
+
+      boardPositions[cm.GetXBoard(), cm.GetYBoard()] = obj;
+    }
 }
