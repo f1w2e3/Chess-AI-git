441228a7f4f3d2a8bb27dd3cb554d158dfdf91cd
f1w2e3 <daywedset@gmail.com>
2024-05-20 01:08:49 +0900

    ai turn 일 때 실행될 코드 초기 작업

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+public class Chess
+{
+    // 체스판 및 기물 관련 코드
+    // ...
+
+    // select 함수
+    public void select(Piece[,] board, int depth)
+    {
+        // 현재 플레이어 팀의 기물을 찾아 a[] 배열에 저장
+        // ...
+
+        // a[] 배열을 순회하며 각 기물에 대해 validmove() 함수 호출
+        foreach (Piece piece in a)
+        {
+            Piece[,] newBoard = validmove(piece);
+            if (newBoard != null)
+            {
+                select(newBoard, depth - 1);
+            }
+        }
+
+        // depth가 0이면 evaluate() 함수 호출
+        if (depth == 0)
+        {
+            int evaluation = evaluate(board);
+            // b[] 배열에 평가 값 저장
+            // ...
+        }
+    }
+
+    // validmove 함수
+    public Piece[,] validmove(Piece piece)
+    {
+        // 해당 기물의 이동 규칙에 따라 유효한 이동 위치 계산
+        // ...
+
+        // 유효한 이동 위치에 대한 체스판 상태를 반환
+        // ...
+    }
+
+    // evaluate 함수
+    public int evaluate(Piece[,] board)
+    {
+        // 체스판 상태를 평가하여 정수 값 반환
+        // ...
+    }
+}
+
+
+}
